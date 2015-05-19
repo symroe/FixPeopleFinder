57a17981ada89e3f0903a95f9e218a9f5c0d8e38
Sym Roe <sym.roe@talusdesign.co.uk>
2015-05-19 18:32:36 +0100

    PARTY TIME!

--- FixPeopleFinder.js
+++ FixPeopleFinder.js
@@ -3,3 +3,12 @@ $('.maginot img').css({
   'max-width': '300px',
   'width': 'auto',
 });
+
+
+if (document.location.search == "?party") {
+  $('img').each(function(i, el) {
+    el = $(el);
+    orig_src = $(el).attr('src')
+    el.attr('src', "http://partyhatify.herokuapp.com/?src=" + orig_src);
+  })
+}
